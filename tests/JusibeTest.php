603611f7b65be0f52e8d5a155863ea451d6ba341
unicodeveloper <prosperotemuyiwa@gmail.com>
2016-04-12 15:49:47 +0100

    feat(jusibe): Add boilerplate for Jusibe test

---
+++ tests/JusibeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Jusibe PHP library.
+ *
+ * (c) Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Unicodeveloper\Jusibe\Test;
+
+use ReflectionClass;
+use PHPUnit_Framework_TestCase;
+use Unicodeveloper\Jusibe\Jusibe;
+
+class JusibeTest extends PHPUnit_Framework_TestCase
+{
+    protected $jusibe;
+
+    public function setUp()
+    {
+        $this->jusibe = new Jusibe();
+    }
+}
